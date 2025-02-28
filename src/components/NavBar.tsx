073996164d1f78e7029bf4219642e53bd718e65a
DananjayaBC <dana.chanuka@gmail.com>
2025-02-28 12:57:51 +0530

    logo update 4

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@
 
 import clsx from "clsx";
 import React, { useState } from "react";
-import { Content, KeyTextField, asLink } from "@prismicio/client";
+import { Content, asLink } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
 import { MdMenu, MdClose } from "react-icons/md";
